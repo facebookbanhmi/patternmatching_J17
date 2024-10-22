de74a3444942e29de47b4c2494a4123dda1fa484
facebookbanhmi <hongtran4phuong@gmail.com>
2024-10-23 01:18:20 +0700

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle implements Shape {
+    final double length;
+    final double width;
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    double length() { return length; }
+    double width() { return width; }
+}
